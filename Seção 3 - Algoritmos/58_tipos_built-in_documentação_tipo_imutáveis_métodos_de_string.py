504958b41262f133bec3ee4118f22cfee5734153
Nayan Couto <pereira.nayan@gmail.com>
2023-06-21 19:17:19 -0300

    aula 0.3.60

---
+++ Seção 3 - Algoritmos/58_tipos_built-in_documentação_tipo_imutáveis_métodos_de_string.py
@@ -0,0 +1,9 @@
+"""
+https://docs.python.org/pt-br/3/library/stdtypes.html
+Imutáveis que foram vistos: str, int, float, bool
+"""
+string = 'Nayan Couto'
+outra_variavel = f'{string[:3]}ABC{string[4:]}'
+print(string)
+print(outra_variavel)
+print(string.casefold())
